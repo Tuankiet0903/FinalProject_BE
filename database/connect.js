1ca0053a0adfb3c46676612c2d9be17defa61ba9
TuanKiet0903 <htk09032003@gmail.com>
2025-01-07 22:46:55 +0700

    Update config DB cloud

--- database/connect.js
+++ database/connect.js
@@ -4,32 +4,43 @@ import { Sequelize } from "sequelize";
 dotenv.config();
 
 // Tạo kết nối tới cơ sở dữ liệu PostgreSQL
-const sequelize = new Sequelize(
-  process.env.DB_NAME,          // Tên cơ sở dữ liệu
-  process.env.DB_USERNAME,      // Tên đăng nhập
-  process.env.DB_PASSWORD,      // Mật khẩu
-  {
-    host: process.env.DB_HOST,  // Host của cơ sở dữ liệu
-    dialect: process.env.DB_DIALECT || "postgres", // Loại cơ sở dữ liệu (mặc định: PostgreSQL)
-    pool: {
-      max: 5,                   // Số kết nối tối đa trong pool
-      min: 0,                   // Số kết nối tối thiểu trong pool
-      idle: 10000,              // Thời gian tối đa một kết nối có thể nhàn rỗi (ms)
-    },
-    logging: false,             // Tắt log SQL trong môi trường sản xuất
-  }
-);
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,          // Tên cơ sở dữ liệu
+//   process.env.DB_USERNAME,      // Tên đăng nhập
+//   process.env.DB_PASSWORD,      // Mật khẩu
+//   {
+//     host: process.env.DB_HOST,  // Host của cơ sở dữ liệu
+//     dialect: process.env.DB_DIALECT || "postgres", // Loại cơ sở dữ liệu (mặc định: PostgreSQL)
+//     pool: {
+//       max: 5,                   // Số kết nối tối đa trong pool
+//       min: 0,                   // Số kết nối tối thiểu trong pool
+//       idle: 10000,              // Thời gian tối đa một kết nối có thể nhàn rỗi (ms)
+//     },
+//     logging: false,             // Tắt log SQL trong môi trường sản xuất
+//   }
+// );
 
-// Kết nối tới cơ sở dữ liệu
-const connectDB = async () => {
-  try {
-    await sequelize.authenticate();
-    console.log("Connected to the PostgreSQL database successfully.");
-  } catch (error) {
-    console.error("Error connecting to the database:", error.message);
-    process.exit(1); // Dừng ứng dụng nếu không thể kết nối
-  }
-};
+const sequelize = new Sequelize(process.env.DB_CONNECT_URL, {
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // Bỏ qua kiểm tra chứng chỉ
+      },
+    },
+  });
+  
+  // Kết nối tới cơ sở dữ liệu
+  const connectDB = async () => {
+    try {
+      await sequelize.authenticate();
+      console.log("Connected to the PostgreSQL database successfully.");
+    } catch (error) {
+      console.error("Error connecting to the database:", error.message);
+      process.exit(1); // Dừng ứng dụng nếu không thể kết nối
+    }
+  };
+  
 
 // Đồng bộ cơ sở dữ liệu (nếu cần)
 const syncDatabase = async () => {
