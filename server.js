42fee9ca63efa5e26870d1ef52c970a3273e1745
tanhtt <flywtan@gmail.com>
2025-01-08 14:44:38 +0700

    setup cors

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
 import { connectDB } from './database/connect.js';
+import { corsOptions } from './config/cors.js';
 import UserTestRouter from './router/UserTest.route.js';
 
 dotenv.config();
@@ -10,7 +11,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(morgan('combined')); // Hoặc 'dev' cho log đơn giản hơn
 
